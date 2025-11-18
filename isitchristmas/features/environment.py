af2895a812dc9367cf3de743e0b90b4ff3036432
Pace Thomson <pacethomson@gmail.com>
2025-11-18 16:22:52 -0700

    Upaded envirnment.py's

--- isitchristmas/features/environment.py
+++ isitchristmas/features/environment.py
@@ -1,8 +1,27 @@
 import behave_webdriver
-from behave_webdriver.steps import *
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+    chrome_options = Options()
+    # chrome_options.binary_location = "/usr/bin/chromium-browser"
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+
+    # Use behave-webdriver’s Chrome wrapper
+    context.behave_driver = behave_webdriver.Chrome(chrome_options=chrome_options)
 
 def after_all(context):
-    context.behave_driver.quit()
+    if hasattr(context, "behave_driver"):
+        context.behave_driver.quit()
+
+
+# import behave_webdriver
+# from behave_webdriver.steps import *
+
+# def before_all(context):
+#     context.behave_driver = behave_webdriver.Chrome()
+
+# def after_all(context):
+#     context.behave_driver.quit()
