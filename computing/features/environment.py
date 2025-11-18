faf12781f71593bf1eb2893250347106e072f6d8
Pace Thomson <pacethomson@gmail.com>
2025-11-17 23:30:14 -0700

    Testing with new flags from chat

--- computing/features/environment.py
+++ computing/features/environment.py
@@ -2,7 +2,14 @@ import behave_webdriver
 from behave_webdriver.steps import *
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+    options = Options()
+    options.binary_location = "/usr/bin/chromium-browser"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
+    context.behave_driver = webdriver.Chrome(options=options)
 
 def after_all(context):
     context.behave_driver.quit()
